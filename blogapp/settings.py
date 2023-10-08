b0219eaa27d50bc2ee81dfe8a7b4fea8391fcbb9
Anjan Kumar <119722627+anjanDevOp@users.noreply.github.com>
2023-10-09 00:03:11 +0530

    Update settings.py

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'whitenoise.runserver_nostatic',
+    
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
